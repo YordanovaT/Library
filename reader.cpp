beb818ac1c08c164335ac8691329eb46593e62b0
YordanovaT <tanyaiyord@gmail.com>
2023-12-01 17:33:45 +0200

    Added new method implementation.

--- reader.cpp
+++ reader.cpp
@@ -4,12 +4,20 @@ Reader::Reader(std::string f_name, std::string l_name)
 {
     first_name=f_name;
     last_name=l_name;
-    count_of_books=0;
+    book_lented=0;
 }  
 
 void Reader::get_name()
 {
     std::cout<<"Name: "<<first_name<< " " <<last_name<<std::endl;
+}
+
+void Reader::output_red_books()
+{
+    for(int i=0;i<books_red.size();i++)
+    {
+        std::cout<<"Book "<<books_red[i]->book_name<<"\tAuthor: "<<books_red[i]->author_name<<std::endl;
+    }
 }
 
 Reader::~Reader()
--- reader.h
+++ reader.h
@@ -1,15 +1,20 @@
 #pragma once
 #include <iostream>
 #include <string.h>
+#include "book.h"
+#include <vector>
 
 class Reader{
     private:
         std::string first_name;
         std::string last_name;
+        
     public:
-        int count_of_books;
+        std::vector<Book *>books_red;
+        int book_lented;
         Reader();
         Reader(std::string f_name, std::string l_name);
         ~Reader();
         void get_name();
+        void output_red_books();
 };
